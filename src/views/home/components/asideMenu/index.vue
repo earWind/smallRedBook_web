86a862efd2810d453438e609d55861fb7297028c
earWind <583113636@qq.com>
2020-08-19 18:16:19 +0800

    mod

---
+++ src/views/home/components/asideMenu/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-menu
+    default-active="1"
+    class="el-menu-vertical-demo aside-menu"
+    @open="handleOpen"
+    @close="handleClose"
+    background-color="#304156"
+    text-color="#fff"
+    active-text-color="#409EFF"
+    :collapse="isCollapse"
+  >
+    <el-menu-item index="1">
+      <i class="el-icon-menu"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-menu-item index="3">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-document"></i>
+      <span slot="title">导航三</span>
+    </el-menu-item>
+     <el-submenu index="5">
+      <template slot="title">
+        <i class="el-icon-setting"></i>
+        <span>设置</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">用户管理</el-menu-item>
+        <el-menu-item index="1-2">权限管理</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "AsideMenu",
+  computed: {
+    ...mapState({
+      sidebar: (state) => state.app.sidebar,
+    }),
+    isCollapse() {
+      return !this.sidebar.opened;
+    },
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.el-menu-vertical-demo {
+  border: 0;
+}
+</style>
--- src/views/home/components/index.js
+++ src/views/home/components/index.js
@@ -1,1 +1,3 @@
 export { default as Navbar } from "./Navbar";
+export { default as TagsView } from "./tagsView/index.vue";
+export { default as AsideMenu } from "./asideMenu/index.vue";
